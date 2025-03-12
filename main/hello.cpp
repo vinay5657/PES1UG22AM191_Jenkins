507af31dd6f058b39fde628d700dbfa97b60b60b
vinay5657 <151739860+vinay5657@users.noreply.github.com>
2025-03-12 10:00:50 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Webhook connected successfully \n";
   return 0;
 }
